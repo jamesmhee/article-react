c888f3e0079a283872079b47bdcfe7f437d1d86f
jamesmhee <nontasabs@gmail.com>
2023-08-15 14:48:13 +0700

    edit firebase

--- src/functions/functionArticle.jsx
+++ src/functions/functionArticle.jsx
@@ -1,18 +1,18 @@
 import axios from "axios";
-import { appCheck, getToken } from "../firebase";
+// import { appCheck, getToken } from "../firebase";
 const url = 'https://article-nodejs-api.onrender.com/api'
 
-const generateToken = async () => {
-    try {
-        const Token = await getToken(appCheck)
-        return Token
-    } catch (error) {
-        console.log(error)
-    }
-}
+// const generateToken = async () => {
+//     try {
+//         const Token = await getToken(appCheck)
+//         return Token
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
 
 export const getData = async () => {
-    const token = await generateToken()
+    // const token = await generateToken()
     return await axios.get(url + '/article')
 }
 
